a563fefb243988b49392ab2346be00065098169d
hauk3wu1ff <hauk3wu1ff@gmail.com>
2018-03-20 20:12:47 +0000

    initial project setup of mvnw and .gitignore

--- 1_ECS_Java_Spring_PetClinic/mvnw.cmd
+++ 1_ECS_Java_Spring_PetClinic/mvnw.cmd
@@ -34,7 +34,7 @@
 @REM ----------------------------------------------------------------------------
 
 @REM Begin all REM lines with '@' in case MAVEN_BATCH_ECHO is 'on'
-@echo off
+@REM echo off
 @REM enable echoing my setting MAVEN_BATCH_ECHO to 'on'
 @if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
 
