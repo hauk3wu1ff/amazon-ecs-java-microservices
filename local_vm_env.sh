3a1afc29365a0fe78e1ba8b4050ab802fb28f93b
hauk3wu1ff@gmail.com <hauk3wu1ff@gmail.com>
2018-04-15 19:09:42 +0100

    Better sudo handling

--- local_vm_env.sh
+++ local_vm_env.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
-
+echo "This script sets environment variables, run script with: source ./local_vm_env.sh"
+ 
 # install python-virtualenv package
-yum -y install python-virtualenv
+sudo yum -y install python-virtualenv
 
 # setup python 2.7 virtual environment and activate it
-pythonenvfolder=~/python28
-virtualenv ${pythonenvfolder}
+pythonenvfolder=~/python27
+if [[ ! -d ${pythonenvfolder} ]] ; then virtualenv ${pythonenvfolder} ; fi
 source ${pythonenvfolder}/bin/activate
+
