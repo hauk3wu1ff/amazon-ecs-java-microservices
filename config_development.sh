4dcf3eeb2ab3faaf6e1acf41711fcbf96a615770
hauk3wu1ff@gmail.com <hauk3wu1ff@gmail.com>
2018-04-15 19:39:35 +0100

    Corrected wrong path and pushd command

--- config_development.sh
+++ config_development.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 # location for maven and tools is bin folder in user home directory
-userbin=~\bin
+userbin=~/bin
 [ ! -d $userbin ] && mkdir $userbin
 
 # install java 8, set it as default and remove java 1.7
-yum -y shell ./yum-script
+sudo yum -y shell ./yum-script
  
 #download maven archive and unpack it
-pusd $userbin
+pushd $userbin
 curl http://www-eu.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz | tar xz
 popd
  
